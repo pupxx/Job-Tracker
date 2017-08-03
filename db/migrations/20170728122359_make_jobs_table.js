df03e2603027cf5cc5fffd62f231c8c34ac9b646
Cheryln <cherylnbarber@gmail.com>
2017-08-03 08:25:09 -0700

    Changed order that jobs list again. Found and fixed bug with the expand job function.  Description section was expandin beyond the bottom border.  Fixed.

--- db/migrations/20170728122359_make_jobs_table.js
+++ db/migrations/20170728122359_make_jobs_table.js
@@ -1,13 +1,13 @@
 exports.up = function(knex) {
   return knex.schema.createTable('jobs', (table) => {
     table.increments();
-    table.string('title').notNullable().defaultTo('');
-    table.string('description').notNullable().defaultTo('');
+    table.text('title').notNullable().defaultTo('');
+    table.text('description').notNullable().defaultTo('');
     table.string('contactFirstName').notNullable().defaultTo('');
     table.string('contactLastName').notNullable().defaultTo('');
     table.string('contactEmail').notNullable().defaultTo('');
     table.string('contactPhone').notNullable().defaultTo('');
-    table.string('jobUrl').notNullable().defaultTo('');
+    table.text('jobUrl').notNullable().defaultTo('');
     table.date('dateApplied').notNullable();
 
     table.timestamps(true, true);
--- db/migrations/20170728122828_make_notes_table.js
+++ db/migrations/20170728122828_make_notes_table.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('notes', (table) => {
     table.increments();
-    table.string('note').notNullable().defaultTo('');
+    table.text('note').notNullable().defaultTo('');
       table.integer('job_id').references('jobs.id').onDelete('CASCADE').notNullable();
     table.timestamps(true, true);
   });
