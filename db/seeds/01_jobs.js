7cfd5c750ff72d63db24c19e391160872761899a
Cheryln <cherylnbarber@gmail.com>
2017-08-03 10:53:58 -0700

    Changed format of form positioning.

--- db/seeds/01_jobs.js
+++ db/seeds/01_jobs.js
@@ -4,12 +4,12 @@ exports.seed = function(knex) {
       return knex('jobs').insert([{
 		 id: 1,
         title: 'First Job applied For',
-        description: 'Some type of description here',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         contactFirstName: 'Joe',
         contactLastName: 'Bob',
         contactEmail: 'joebob@gmail.com',
         contactPhone: '555-555-5555',
-        dateApplied: '07/31/2017',
+        dateApplied: '06/15/2017',
         jobUrl: 'http://www.ourfavoriterecipes.org',
         created_at: new Date(),
         updated_at: new Date()
@@ -21,7 +21,7 @@ exports.seed = function(knex) {
         contactLastName: 'Smith',
         contactEmail: 'sallysmith@gmail.com',
         contactPhone: '444-444-4444',
-        dateApplied: '06/15/2017',
+        dateApplied: '07/15/2017',
         jobUrl: 'http://www.indeed.com',
         created_at: new Date(),
         updated_at: new Date()
