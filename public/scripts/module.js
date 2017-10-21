fa2ea9b8b48809edbc20abaa4e8c4e00bdda83e7
Cheryln <cherylnbarber@gmail.com>
2017-10-21 12:27:30 -0700

    Fixed spelling

--- public/scripts/module.js
+++ public/scripts/module.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict'
   angular.module('app', [ 'ui.router', 'angularMoment'])
-   .constant('API_BASE_URL', 'https://cbjob-tracker.herokuapp.com/view-all-jobs)
+   .constant('API_BASE_URL', 'https://cbjob-tracker.herokuapp.com/view-all-jobs')
   // .constant('API_BASE_URL', 'http://localhost:4000')
 })()
